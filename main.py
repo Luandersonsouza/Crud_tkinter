2e489e35c1d0171ac27e68af43c9252cdd4a7916
Luandersonsouza <jlgs@ic.ufal.br>
2022-01-24 12:34:58 -0300

    crud finalizado

--- main.py
+++ main.py
@@ -2,7 +2,53 @@ from tkinter import *
 from tkinter import ttk
 import sqlite3
 
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Image
+import webbrowser
+
+from PIL import ImageTk, Image
+
+
 window = Tk()
+
+
+class reports():
+	def print_customers(self):
+		webbrowser.open("customer.pdf")
+
+
+	def generate_customer_report(self):
+		self.c = canvas.Canvas("customer.pdf")
+
+		self.code_report = self.code_entry.get()
+		self.name_report = self.name_entry.get()
+		self.phone_report = self.phone_entry.get()
+		self.city_report = self.city_entry.get()
+
+		self.c.setFont("Helvetica-Bold", 24)
+		self.c.drawString(200, 790, 'Customer File')
+
+		self.c.setFont("Helvetica-Bold", 18)
+		self.c.drawString(50, 700,'Code : ')
+		self.c.drawString(50, 670,'Name : ')
+		self.c.drawString(50, 630,'Phone : ')
+		self.c.drawString(50, 600,'City : ')
+
+		self.c.setFont("Helvetica", 18)
+		self.c.drawString(150, 700, self.code_report)
+		self.c.drawString(150, 670, self.name_report)
+		self.c.drawString(150, 630, self.phone_report)
+		self.c.drawString(150, 600, self.city_report)
+
+		self.c.rect(20,550,550,200, fill = False, stroke = True)
+
+		self.c.showPage()
+		self.c.save()
+		self.print_customers()
+
 class functions():
 	def clear_frame_1(self):
 		self.code_entry.delete(0, END)
@@ -34,33 +80,90 @@ class functions():
 		self.db_disconnection()
 
 
-	def add_customer(self):
+	def variables(self):
 		self.code = self.code_entry.get()
 		self.name = self.name_entry.get()
 		self.phone = self.phone_entry.get()
 		self.city = self.city_entry.get()
 
+
+	def add_customer(self):
+
+		self.variables()
+
 		self.db_connection()
 
-		self.cursor.execute(""" INSERT INTO customers (name_customers, phone, city)
-				VALUES (?,?,?)""", (self.name, self.phone, self.city))
+		self.cursor.execute(""" INSERT INTO 
+								customers (name_customers, phone, city)
+								VALUES (?,?,?)""", 
+								(self.name, self.phone, self.city))
 		self.conn.commit()
 		self.db_disconnection()
-		self.select_list()
+		self.select()
 		self.clear_frame_1()
 
 
-	def select_list(self):
+	def select(self):
 		self.listaCli.delete(*self.listaCli.get_children())
 		self.db_connection()
-		lista = self.cursor.execute("""	SELECT code, name_customers, phone, city FROM customers
-					ORDER BY name_customers ASC; """)
-		for i in lista:
+		list_a = self.cursor.execute("""	SELECT code, name_customers, phone, city 
+											FROM customers
+											ORDER BY name_customers ASC; 
+									""")
+		for i in list_a:
 			self.listaCli.insert("", END, values=i)
-			self.db_disconnection()
+		self.db_disconnection()
 
 
-class aplication(functions):
+	def on_double_click(self, event):
+		self.clear_frame_1()
+		self.listaCli.selection()
+
+		for n in self.listaCli.selection():
+			col1,col2,col3,col4 = self.listaCli.item(n, 'values')
+			self.code_entry.insert(END, col1)
+			self.name_entry.insert(END, col2)
+			self.phone_entry.insert(END, col3)
+			self.city_entry.insert(END, col4)
+
+
+	def delete_customer(self):
+		self.variables()
+		self.db_connection()
+		self.cursor.execute("""DELETE FROM customers WHERE code = ?  """, (self.code))
+		self.conn.commit()
+		self.db_disconnection()
+		self.clear_frame_1()
+		self.select()
+
+
+	def change_customers(self):
+		self.variables()
+		self.db_connection()
+		self.cursor.execute(""" UPDATE customers SET name_customers = ?, phone = ?, city = ?
+							WHERE code = ?""", (self.name, self.phone, self.city, self.code))
+		self.conn.commit()
+		self.db_disconnection()
+		self.select()
+		self.clear_frame_1()
+
+
+	def search_customer (self):
+		self.db_connection()
+		self.listaCli.delete(*self.listaCli.get_children())
+		self.name_entry.insert(END, '%')
+		name = self.name_entry.get()
+		self.cursor.execute(""" SELECT code, name_customers, phone, city
+								FROM customers 
+								WHERE name_customers LIKE '%s' ORDER BY name_customers ASC
+							""" %name)
+		search_name_customer = self.cursor.fetchall()
+		for i in search_name_customer:
+			self.listaCli.insert("", END, values =i)
+		self.clear_frame_1()
+		self.db_disconnection()
+
+class aplication(functions, reports):
 	def __init__(self):
 		self.window = window
 		self.screen()
@@ -68,7 +171,8 @@ class aplication(functions):
 		self.widgets_Frame_1()
 		self.widgets_Frame_2()
 		self.create_tables_db()
-		self.select_list()
+		self.select()
+		self.menu()
 		window.mainloop()
 
 
@@ -93,30 +197,35 @@ class aplication(functions):
 	def widgets_Frame_1(self):
 		#creating the clear button
 		self.button_clear = Button(self.frame_1, text="Clear", bd = 2, bg = '#107db2',
-									 fg = 'white', font = ('verdana', 8, 'bold'), command = self.clear_frame_1)
+									 fg = 'white', activebackground = '#108ecb', activeforeground = 'white',
+									 font = ('verdana', 8, 'bold'),command = self.clear_frame_1)
 		self.button_clear.place(relx= 0.2, rely= 0.15,relwidth=0.1, relheight=0.15)
 
 		#creanting the search button
 		self.button_search = Button(self.frame_1, text="Search",bd = 2, bg = '#107db2',
-									 fg = 'white', font = ('verdana', 8, 'bold'))
+									 fg = 'white', activebackground = '#108ecb', activeforeground = 'white',
+									 font = ('verdana', 8, 'bold'), command = self.search_customer)
 		self.button_search.place(relx= 0.3, rely= 0.15,relwidth=0.1, relheight=0.15)
 
 
 		#creating the new button
 		self.button_new = Button(self.frame_1, text="New",bd = 2, bg = '#107db2',
-									 fg = 'white', font = ('verdana', 8, 'bold'), command= self.add_customer)
+									 fg = 'white', activebackground = '#108ecb', activeforeground = 'white',
+									 font = ('verdana', 8, 'bold'), command= self.add_customer)
 		self.button_new.place(relx= 0.6, rely= 0.15,relwidth=0.1, relheight=0.15)
 
 
 		#creating the change button
 		self.button_change = Button(self.frame_1, text="Change", bd = 2, bg = '#107db2',
-									 fg = 'white', font = ('verdana', 8, 'bold'))
+									 fg = 'white', activebackground = '#108ecb', activeforeground = 'white',
+									 font = ('verdana', 8, 'bold'), command = self.change_customers)
 		self.button_change.place(relx= 0.7, rely= 0.15,relwidth=0.1, relheight=0.15)
 
 
 		#creating the delete button
 		self.button_delete = Button(self.frame_1, text="Delete", bd = 2, bg = '#107db2',
-									 fg = 'white', font = ('verdana', 8, 'bold'))
+									 fg = 'white', activebackground = '#108ecb', activeforeground = 'white', 
+									 font = ('verdana', 8, 'bold'), command = self.delete_customer)
 		self.button_delete.place(relx= 0.8, rely= 0.15,relwidth=0.1, relheight=0.15)
 
 
@@ -164,9 +273,32 @@ class aplication(functions):
 
 		self.listaCli.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
 
-		self.scrool_lista = Scrollbar(self.frame_2, orient='vertical')
-		self.listaCli.configure(yscroll=self.scrool_lista.set)
-		self.scrool_lista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight= 0.85)
+		self.scroll_lista = Scrollbar(self.frame_2, orient='vertical')
+		self.listaCli.configure(yscroll=self.scroll_lista.set)
+		self.scroll_lista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight= 0.85)
+
+		self.listaCli.bind("<Double-1>", self.on_double_click)
+
+
+	def menu (self):
+		menu_bar = Menu(self.window)
+		self.window.config(menu = menu_bar)
+		file_menu = Menu(menu_bar)
+		file_menu_2 = Menu(menu_bar)
+
+
+		def quit(): self.window.destroy()
+
+
+		menu_bar.add_cascade(label = "Options", menu = file_menu)
+		menu_bar.add_cascade(label = "Reports", menu = file_menu_2)
+
+
+		file_menu.add_command(label = "Quit", command = quit)
+		file_menu.add_command(label = "Clear frame", command = self.clear_frame_1)
+
+
+		file_menu_2.add_command(label = "Customer File", command = self.generate_customer_report)
 
 
 
